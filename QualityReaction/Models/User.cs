6689a6c2f72f7fbaf40fca22776208c6a96f19d6
Pac <toopac@hotmail.com>
2019-01-28 18:04:20 +0100

    Suppression imports innutiles

--- QualityReaction/Models/User.cs
+++ QualityReaction/Models/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace QualityReaction.Models
+﻿namespace QualityReaction.Models
 {
     public class User
     {
